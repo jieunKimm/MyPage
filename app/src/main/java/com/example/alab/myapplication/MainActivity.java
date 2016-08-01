569b3ff95e04604b63238a34361c2a5c7fc79dc3
alab <qorentks7777@gmail.com>
2016-08-01 11:11:03 +0800

    add onclick

--- app/src/main/java/com/example/alab/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alab/myapplication/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.alab.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.net.Uri;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    public void  (View view) {
+        goToUrl("http://www.cgv.co.kr/culture-event/event/detail-view.aspx?idx=13641&menu=4");
     }
 }
